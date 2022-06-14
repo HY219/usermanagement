f6480076135832ba708c9c312af3380833bb98d7
hye219 <jhy_0219@daum.net>
2022-06-14 17:48:52 +0900

    프로젝트 생성

---
+++ src/main/java/kr/ac/jejunu/usermenagement/UsermenagementApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.jejunu.usermenagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsermenagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsermenagementApplication.class, args);
+    }
+
+}
