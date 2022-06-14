bf5b28363f916c75a74743927198f6fa99278ec7
hye219 <jhy_0219@daum.net>
2022-06-14 17:57:10 +0900

    list 페이지 띄우기

---
+++ src/main/java/kr/ac/jejunu/usermenagement/MvcConfiguration.java
@@ -0,0 +1,14 @@
+package kr.ac.jejunu.usermenagement;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfiguration implements WebMvcConfigurer {
+    //html을 띄우기 위한 과정(@Override addViewControllers)
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/list");
+    }
+}
